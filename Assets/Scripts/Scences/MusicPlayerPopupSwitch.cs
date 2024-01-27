d8ed42c8c7fca73aab2e5c0068823d27efbd0bbf
ooncakemay <ooncakemay@gmail.com>
2024-01-27 20:14:14 +0800

    播放器的switch

---
+++ Assets/Scripts/Scences/MusicPlayerPopupSwitch.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scenes
+{
+    public class MusicPlayerPopupSwitch:MonoBehaviour
+    {
+        
+        public GameObject pop;
+        public Button click;
+
+        public void Start()
+        {
+            click.onClick.AddListener(Switch);
+        }
+
+        private void Switch()
+        {
+           pop.SetActive(!pop.activeSelf);
+        }
+    }
+}
