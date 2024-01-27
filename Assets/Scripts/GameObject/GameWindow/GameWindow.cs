3c62784be0aca09fdcdaa7f4167d5cee0be6642b
Aotokitsuruya <contact@aotoki.me>
2024-01-27 18:59:29 +0800

    Make unlocked loading progress open result
    Former-commit-id: 9cf8d473e71eea39ff891ecd9393e39b76be5019

--- Assets/Scripts/GameObject/GameWindow/GameWindow.cs
+++ Assets/Scripts/GameObject/GameWindow/GameWindow.cs
@@ -1,16 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Command;
 using Entity;
+using Event;
 using Reflex.Attributes;
 using UnityEngine;
 
-public class GameWindow : MonoBehaviour
+public class GameWindow : MonoBehaviour, IGameEventHandler<UnlockEvent>
 {
+    public GameObject loadingScreen;
+    public GameObject resultScreen;
+    
+    [Inject] private readonly GameEvent<UnlockEvent> _unlockEvent; 
     [Inject] private readonly PuzzleCommand _puzzleCommand;
-    // Start is called before the first frame update
+
     void Start()
     {
+        _unlockEvent.AddListener(this);
         _puzzleCommand.Start(PuzzleType.LoadingScreen, Time.time);
+    }
+    
+    void OnDestroy()
+    {
+        _unlockEvent.RemoveListener(this);
+    }
+
+    public void OnGameEvent(UnlockEvent payload)
+    {
+        if(payload.type == LockType.LoadingProgress)
+        {
+            loadingScreen.SetActive(false);
+            resultScreen.SetActive(true);
+        }
     }
 }
