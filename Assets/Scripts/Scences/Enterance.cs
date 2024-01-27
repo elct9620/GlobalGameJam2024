f66b7eef11e6a92724da9dc7cbda6754f383f9d8
Aotokitsuruya <contact@aotoki.me>
2024-01-27 11:48:46 +0800

    Add StartButton event

--- Assets/Scripts/Scences/Enterance.cs
+++ Assets/Scripts/Scences/Enterance.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Reflex.Attributes;
+using UnityEngine.SceneManagement;
 
 public class Enterance : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Level1");
     }
 }
