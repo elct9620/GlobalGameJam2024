10c8d457b40ef31cd8a3eb87479beab253052c09
ooncakemay <ooncakemay@gmail.com>
2024-01-27 17:44:42 +0800

    [更新]音樂播放器unlock

---
+++ Assets/Scripts/GameObject/MusicButton.cs
@@ -0,0 +1,33 @@
+﻿
+using Command;
+using Entity;
+using Reflex.Attributes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicButton:MonoBehaviour
+{
+    [Inject] private readonly UnlockCommand _unlockCommand;
+
+    public GameObject turnOn;
+    public GameObject turnOff;
+    public void Start()
+    {
+        var button = GetComponent<Button>();
+        
+        button.onClick.AddListener(Unlock);
+        
+    }
+
+    private void Unlock()
+    {
+        var result = _unlockCommand.Unlock(LockType.MusicPlaying,Time.time);
+
+        if (result)
+        {
+            turnOff.SetActive(true);
+            turnOn.SetActive(false);
+        }
+
+    }
+}
