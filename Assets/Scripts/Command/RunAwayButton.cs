c214a0686379e1643d30cbb54354958b1e7263d4
jacksonhuang47 <157859622+jacksonhuang47@users.noreply.github.com>
2024-01-27 18:10:07 +0800

    fixed RunAwayButton

--- Assets/Scripts/Command/RunAwayButton.cs
+++ Assets/Scripts/Command/RunAwayButton.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class RunAwayButton : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 40f;
 
     void Update()
     {
-        Vector2 mousePosition = Input.mousePosition;
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 0f));
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = -Camera.main.transform.position.z;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        Vector2 moveDirection = (Vector2)transform.position - (Vector2)mouseWorldPosition;
+        Vector3 moveDirection = mousePosition - transform.position;
+
+
+        moveDirection.z = 0;
+
         moveDirection.Normalize();
 
-
-        Vector2 newPosition = (Vector2)transform.position + moveDirection * speed * Time.deltaTime;
-
-        newPosition.x = Mathf.Clamp(newPosition.x, Camera.main.ViewportToWorldPoint(Vector3.zero).x, Camera.main.ViewportToWorldPoint(Vector3.one).x);
-        newPosition.y = Mathf.Clamp(newPosition.y, Camera.main.ViewportToWorldPoint(Vector3.zero).y, Camera.main.ViewportToWorldPoint(Vector3.one).y);
-
-        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+        transform.Translate(-moveDirection * speed * Time.deltaTime);
     }
 }
-
 
 
