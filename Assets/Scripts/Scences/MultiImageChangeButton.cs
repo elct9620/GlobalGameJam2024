eb6de8b3d07889574ef5d5231445aeffa587c011
Scott50266 <sasdqsdq@gmail.com>
2024-01-27 13:29:12 +0800

    陽春按鈕換圖片的功能

---
+++ Assets/Scripts/Scences/MultiImageChangeButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scenes
+{
+    public class MultiImageChangeButton:MonoBehaviour
+    {
+        public List<Button> Buttons;
+        public Image Image;
+        public List<Sprite> Sprites;
+        
+        public void Start()
+        {
+
+            if (Buttons.Count != Sprites.Count)
+            {
+                throw new Exception("圖片數量不對");
+            }
+
+            for (var index = 0; index < Buttons.Count; index++)
+            {
+                var test = index;
+                var button = Buttons[index];
+                button.onClick.AddListener(()=> ChangeImage(test,button));
+            }
+        }
+        
+        void ChangeImage(int index,Button button)
+        {
+            //Output this to console when the Button3 is clicked
+            Image.sprite = Sprites[index];
+            button.interactable = false;
+
+        }
+    }
+}
