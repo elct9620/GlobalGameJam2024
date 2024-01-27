a47a7f59e09e413973cef73a20205fc255f5f246
Aotokitsuruya <contact@aotoki.me>
2024-01-27 13:04:09 +0800

    Throw error when a puzzle is started

--- Assets/Scripts/Command/Puzzle.cs
+++ Assets/Scripts/Command/Puzzle.cs
@@ -1,3 +1,4 @@
+using System;
 using Reflex.Attributes;
 using Repository;
 using UnityEngine.SceneManagement;
@@ -5,6 +6,10 @@ using Score = Repository.Score;
 
 namespace Command
 {
+    public class PuzzleStartError : Exception
+    {
+        
+    }
     public class Puzzle {
         private readonly Score _score;
         private readonly Game _game;
@@ -22,6 +27,11 @@ namespace Command
 
         public void Start(string name, double time)
         {
+            if (_game.CurrentPuzzle != null)
+            {
+                throw new PuzzleStartError();
+            }
+            
             Entity.Puzzle puzzle = new Entity.Puzzle(name, time);
             _game.SetPuzzle(puzzle);
         }
