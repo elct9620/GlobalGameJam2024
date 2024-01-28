0ad34b5924a0d2a18323d4804bb478381ffc7599
Aotokitsuruya <contact@aotoki.me>
2024-01-28 13:36:31 +0800

    Improve game result display

--- Assets/Scripts/Entity/PuzzleType.cs
+++ Assets/Scripts/Entity/PuzzleType.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel;
+
 namespace Entity
 {
     public enum PuzzleType
     {
        None,
+       [Description("Boot Program")]
        BootProgram,
+       [Description("Make Progress Advance")]
        LoadingScreen,
+       [Description("Defeat the Boss")]
        GameScreen,
     }
 }
--- Assets/Scripts/GameObject/GameWindow/ResultItem.cs
+++ Assets/Scripts/GameObject/GameWindow/ResultItem.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
 using Entity;
 using TMPro;
 using UnityEngine;
@@ -10,20 +13,32 @@ public class ResultItem : MonoBehaviour
     public TMP_Text puzzleName;
     public TMP_Text score;
     public Slider filled;
-    
+
     public void SetResultItem(PuzzleType puzzleType, double score, double maxScore)
     {
-        puzzleName.text = puzzleType.ToString();
+        puzzleName.text = GetDescriptionAttribute(puzzleType);
         this.score.text = FormatScore(score);
-        filled.value = (float) (score / maxScore);
+        filled.value = (float)(score / maxScore);
     }
 
     private static string FormatScore(double score)
     {
-        int hour = (int) score / 3600;
-        int minute = (int) (score % 3600) / 60;
-        int second = (int) (score % 3600) % 60;
-        
+        int hour = (int)score / 3600;
+        int minute = (int)(score % 3600) / 60;
+        int second = (int)(score % 3600) % 60;
+
         return $"{hour:D2}:{minute:D2}:{second:D2}";
+    }
+
+    private static string GetDescriptionAttribute(PuzzleType puzzleType)
+    {
+        string enumName = puzzleType.ToString();
+        FieldInfo fieldInfo = puzzleType.GetType().GetField(enumName);
+        if (fieldInfo.GetCustomAttribute(typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+        {
+            return attribute.Description;
+        }
+
+        return enumName;
     }
 }
